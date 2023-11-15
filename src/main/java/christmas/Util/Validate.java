3e7c494721caba0dd15b08dda2179672b6af2b17
minju <minju9711@naver.com>
2023-11-15 17:27:46 +0900

    [feat] 클라이언트가 Menu 주문시 입력값이 올바른 형태인지 확인해주는 함수 추가.

--- src/main/java/christmas/Util/Validate.java
+++ src/main/java/christmas/Util/Validate.java
@@ -7,7 +7,6 @@ import static christmas.Constant.ValidateConstant.UNCORRECTORDER;
 
 public class Validate {
 
-    //DateInput 구현
     public static int DateInput(String Date) throws IllegalArgumentException {
         int date;
         try{
@@ -19,6 +18,18 @@ public class Validate {
             throw new IllegalArgumentException(UNCORRECTDATE + " (1~31까지의 숫자만 넣어주세요.)");
         }
         return date;
+    }
+
+    public static void MenuFormInput(String OrderInput) throws IllegalArgumentException { //
+        boolean hasInvalidDelimiter = OrderInput.matches(".*[^\\d,\\-,\\p{IsHangul}].*");
+        //한글, 숫자, 쉼표(,), 대시(-)를 제외한 문자 포함시, true 반환
+        if(hasInvalidDelimiter || !OrderInput.contains("-")){
+            throw new IllegalArgumentException(UNCORRECTORDER+" (메뉴입력 형태가 잘못되었습니다.)\" ");
+        }
+    }
+
+    public static void MenuNameInput(){
+        throw new IllegalArgumentException(UNCORRECTORDER + " (메뉴가 존재하지 않습니다.)\n");
     }
 
 }
