ca26c03b0e8cb16c277b4082e50c03fba6a1c6fe
minju <minju9711@naver.com>
2023-11-15 17:04:46 +0900

    [feat] 증정품에 대한 정보를 담은 상수들의 집합 클래스인 GiftConstant 추가

---
+++ src/main/java/christmas/Constant/GiftConstant.java
@@ -0,0 +1,10 @@
+package christmas.Constant;
+
+public class GiftConstant {
+    public static final String GIFTNAME = "샴페인";
+    public static final Integer GIFTPRICE = 25000;
+    public static final Integer GIFTQUANTITY = 1;
+    public static final Integer GIFTSTANDARD = 12000;
+    public static final Boolean NOGIFT = false;
+    public static final Boolean YESGIFT = true;
+}
