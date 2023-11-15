930a16e9e1d4def5f6011e7ee40e01a09868ae28
minju <minju9711@naver.com>
2023-11-15 18:28:38 +0900

    [feat] 배지의 정보와 총할인가격에 따라 배지를 반환해주는 enum 클래스.

---
+++ src/main/java/christmas/Discount/Badge.java
@@ -0,0 +1,24 @@
+package christmas.Discount;
+
+public enum Badge { //상태와 행위를 한곳에서 관리
+    없음(0),
+    별(5000),
+    나무(10000),
+    산타(20000);
+    private final int minDiscount;
+
+    Badge(int minDiscount) {
+        this.minDiscount = minDiscount;
+    }
+
+    public static Badge getBadge(int discount) {
+        Badge[] badges = Badge.values();
+        for (int i = badges.length - 1; i >= 0; i--) {
+            if (discount >= badges[i].minDiscount) {
+                return badges[i];
+            }
+        }
+        return 없음;
+    }
+
+}
