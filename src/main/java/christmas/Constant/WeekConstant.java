20c30c6664804dbc060e3a664d606c583aa0a093
minju <minju9711@naver.com>
2023-11-15 17:12:43 +0900

    [feat] 평일과 주말에 대한 정보를 담은 WeekConstant 추가 : WeekType enum 클래스 생성시 해당 정보를 사용. (만약 평일과 주말에 대한 할인 정보가 바뀔 경우를 대비해 해당 상수클래스를 생성해 실제 비즈니스 로직에서는 상수로만 이용함)

---
+++ src/main/java/christmas/Constant/WeekConstant.java
@@ -0,0 +1,13 @@
+package christmas.Constant;
+
+public class WeekConstant {
+    //평일
+    public static final String WEEKDAYTARGET = "DESSERT";
+    public static final String WEEKDAYDESCRIPTION = "평일 할인. 디저트 메뉴 1개당 할인이 적용됩니다.";
+    public static final Integer WEEKDISCOUNT = 2023;
+
+    //주말
+    public static final String WEEKENDTARGET = "MAIN";
+    public static final String WEEKENDDESCRIPTION = "주말 할인. 메인 메뉴 1개당 할인이 적용됩니다.";
+    public static final Integer WEEKENDDISCOUNT = 2023;
+}
