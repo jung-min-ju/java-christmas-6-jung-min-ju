b63d1bfb3572e9ac7f8f472ffb76f5e92c3f1ae7
minju <minju9711@naver.com>
2023-11-15 17:05:34 +0900

    [feat] 메뉴에 대한 정보를 담은 상수들의 집합 클래스인 MenuConstant 추가

---
+++ src/main/java/christmas/Constant/MenuConstant.java
@@ -0,0 +1,20 @@
+package christmas.Constant;
+
+public class MenuConstant {
+    public static final String MAIN = "티본스테이크-55000,바비큐립-54000,해산물파스타-35000,크리스마스파스타-25000";
+    public static final String APITEZER = "양송이수프-6000,타파스-5500,시저샐러드-8000";
+    public static final String DESSERT = "초코케이크-15000,아이스크림-5000";
+    public static final String DRINK = "제로콜라-3000,레드와인-60000,샴페인-25000";
+
+    public static final String MENU = "<애피타이저>" + System.lineSeparator() +
+            "양송이수프(6,000), 타파스(5,500), 시저샐러드(8,000)" + System.lineSeparator() +
+            System.lineSeparator() +
+            "<메인>" + System.lineSeparator() +
+            "티본스테이크(55,000), 바비큐립(54,000), 해산물파스타(35,000), 크리스마스파스타(25,000)" + System.lineSeparator() +
+            System.lineSeparator() +
+            "<디저트>" + System.lineSeparator() +
+            "초코케이크(15,000), 아이스크림(5,000)" + System.lineSeparator() +
+            System.lineSeparator() +
+            "<음료>" + System.lineSeparator() +
+            "제로콜라(3,000), 레드와인(60,000), 샴페인(25,000)" + System.lineSeparator();
+}
