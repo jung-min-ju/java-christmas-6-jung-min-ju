59acaeb0d1f65f77892a457f0d5112b3188d872e
minju <minju9711@naver.com>
2023-11-15 17:44:22 +0900

    [feat] 사용자가 입력한 날짜에 대해, 그 날의 혜택을 미리 한 줄로 보여줄 로직을 가진 TodayBenefits 함수

---
+++ src/main/java/christmas/Discount/DiscountServiceImpl.java
@@ -0,0 +1,31 @@
+package christmas.Discount;
+
+import christmas.IO.OutputView;
+import java.util.List;
+
+import static christmas.Constant.DateConstant.CHRISTMAS;
+import static christmas.Constant.DiscountConstant.*;
+
+public class DiscountServiceImpl implements DiscountService {
+    private StarStatus starstatus;
+    private WeekType weekType;
+    private int ChristDiscount = 0;
+    private int TotalWeekDiscount=0;
+
+    @Override
+    public void TodayBenefits(int dateInput){
+        starstatus = StarStatus.determineStatus(dateInput);
+        weekType =  weekType.determineWeekStatus(dateInput);
+        ChristDiscount = ChristmasDiscount(dateInput);
+
+        OutputView.PrintTodayBenefits(ChristDiscount,starstatus,weekType);
+    }
+
+    private int ChristmasDiscount(int dateInput){
+        if(dateInput>CHRISTMAS){
+            return CHRISTMASEVENTEND;
+        }
+        return CHRISTBASICDISCOUNT + ((dateInput-1)*100);
+    }
+
+}
