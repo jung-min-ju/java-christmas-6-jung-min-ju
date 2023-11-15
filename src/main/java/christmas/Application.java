7648cc55b7d7f2ef116b2d595c78e4d56dbd440a
minju <minju9711@naver.com>
2023-11-15 17:15:47 +0900

    [feat] 출력문에 대한 예시를 참고해 전체적인 기능을 3가지로 나누고, Controller 클래스에서 이를 구현예정.

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,15 @@
 package christmas;
 
+import christmas.Controller.Controller;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        AppConfig appConfig = new AppConfig();
+        Controller controller = appConfig.controller();
+
+        //클라이언트 역할
+        controller.Reservation();
+        controller.ShowMenuAndTotalPrice();
+        controller.ShowBenefits();
     }
 }
