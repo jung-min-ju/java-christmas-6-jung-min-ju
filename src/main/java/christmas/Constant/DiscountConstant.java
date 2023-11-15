b9a404dff678f82d17a85afe4a072eac8e855af7
minju <minju9711@naver.com>
2023-11-15 17:04:11 +0900

    [feat] 할인을 나타내는 상수들의 집합 클래스인 DiscountConstant 추가

---
+++ src/main/java/christmas/Constant/DiscountConstant.java
@@ -0,0 +1,10 @@
+package christmas.Constant;
+
+public class DiscountConstant {
+    public static final Integer CHRISTBASICDISCOUNT = 1000;
+    public static final Integer CHRISTMASEVENTEND = 0;
+    public static final boolean STARDAY = true;
+    public static final boolean NONESTARDAY = false;
+    public static final Integer STARDISCOUNT = 1000;
+    public static final Integer EVENTSTANDARD = 10000;
+}
