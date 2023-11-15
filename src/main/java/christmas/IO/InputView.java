40b23274f492773d5f97b1a7121ece9851ceb87b
minju <minju9711@naver.com>
2023-11-15 17:45:26 +0900

    [feat] 사용자에게 주문을 물어볼 AskOrder 함수

--- src/main/java/christmas/IO/InputView.java
+++ src/main/java/christmas/IO/InputView.java
@@ -3,11 +3,15 @@ package christmas.IO;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-
-    //AskVisting 구현
     public static String AskVisiting(){
         OutputView.AskVisiting();
         return Console.readLine();
     }
+
+    public static String AskOrder(){
+        OutputView.AskOrder();
+        return Console.readLine();
+    }
+
 
 }
