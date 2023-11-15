8d7e99804ba17c3c06dcff3e23c3abbf529635f7
minju <minju9711@naver.com>
2023-11-15 17:03:06 +0900

    [feat] 날짜를 나타내는 상수들의 집합 클래스인 DateConstant 추가

---
+++ src/main/java/christmas/Constant/DateConstant.java
@@ -0,0 +1,9 @@
+package christmas.Constant;
+
+public class DateConstant {
+    public static final Integer YEAR = 2023;
+    public static final Integer MONTH = 12;
+    public static final Integer CHRISTMAS = 25;
+    public static final String WEEKDAY = "WEEKDAY";
+    public static final String WEEKEND = "WEEKEND";
+}
