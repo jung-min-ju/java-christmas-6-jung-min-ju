09fbb1282e8406aaca5d16830d8ce8b981982b9b
minju <minju9711@naver.com>
2023-11-15 17:10:13 +0900

    [feat] 예외처리 시 출력문을 담은 상수들의 집합 클래스인 ValidateConstant 추가

---
+++ src/main/java/christmas/Constant/ValidateConstant.java
@@ -0,0 +1,7 @@
+package christmas.Constant;
+
+public class ValidateConstant {
+    public static final String UNCORRECTDATE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    public static final String UNCORRECTORDER = "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.";
+
+}
