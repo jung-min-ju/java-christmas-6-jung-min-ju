76df0c1fc33b1d8d4e38f50dbed9b6494184d790
minju <minju9711@naver.com>
2023-11-15 21:26:03 +0900

    [REFACTOR] starday일때의 stardiscount와, nonestarday일때의 stardiscount를 분리한는 상수추가.

--- src/main/java/christmas/Constant/DiscountConstant.java
+++ src/main/java/christmas/Constant/DiscountConstant.java
@@ -5,6 +5,7 @@ public class DiscountConstant {
     public static final Integer CHRISTMASEVENTEND = 0;
     public static final boolean STARDAY = true;
     public static final boolean NONESTARDAY = false;
-    public static final Integer STARDISCOUNT = 1000;
+    public static final Integer EXCISTINGSTARDISCOUNT = 1000;
+    public static final Integer NONESTARDISCOUNT = 0;
     public static final Integer EVENTSTANDARD = 10000;
 }
