87998b43dba03d645441bf23107385794191bdd8
minju <minju9711@naver.com>
2023-11-15 17:42:26 +0900

    [feat] 들어오는 입력값이 주말인지, 평일인지를 판단할 정보를 가진 Enum 클래스 WeekType

---
+++ src/main/java/christmas/Discount/WeekType.java
@@ -0,0 +1,40 @@
+    package christmas.Discount;
+
+    import christmas.Util.JudgeWeek;
+    import static christmas.Constant.DateConstant.WEEKEND;
+    import static christmas.Constant.WeekConstant.*;
+
+    public enum WeekType { //상태와 행위를 한곳에서 관리
+        WEEKDAYTYPE (WEEKDAYTARGET, WEEKDAYDESCRIPTION, WEEKDISCOUNT),
+        WEEKENDTYPE (WEEKENDTARGET, WEEKENDDESCRIPTION, WEEKENDDISCOUNT);
+
+        private final String Target;
+        private final String Description;
+        private final int Discount;
+
+        WeekType(String target, String description, int discount) {
+            this.Target = target;
+            this.Description = description;
+            this.Discount = discount;
+        }
+
+        public static WeekType determineWeekStatus(int DateInput) {
+            String weekType = JudgeWeek.ReturnJudgeWeek(DateInput);
+            if(weekType == WEEKEND)  {
+                return WEEKENDTYPE;
+            }
+            return WEEKDAYTYPE;
+        }
+
+        public String getDescription() {
+            return Description;
+        }
+
+        public int getDiscount() {
+            return Discount;
+        }
+
+        public String getTarget() {
+            return Target;
+        }
+    }
